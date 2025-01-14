dc86c36d35926126625a535d5bd1d6a8d9d8f81b
Carlos Moreno <morenort1402@gmail.com>
2025-01-14 12:01:36 +0100

    FEAT : Create Factories

---
+++ src/Factory/MachineFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Machine;
+use App\Repository\MachineRepository;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<Machine>
+ */
+final class MachineFactory extends PersistentProxyObjectFactory{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Machine::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        $statusOptions = ['Active', 'Inactive', 'On Maintain'];
+        $faker = Factory::create();
+
+        return [
+            'location' => $faker->city,
+            'model' => $faker->word() . ' ' . $faker->randomNumber(3),
+            'status' => $faker->randomElement($statusOptions),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Machine $machine): void {})
+        ;
+    }
+}
---
+++ src/Factory/StockFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Stock;
+use App\Repository\StockRepository;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<Stock>
+ */
+final class StockFactory extends PersistentProxyObjectFactory{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Stock::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        $faker = Factory::create();
+
+        return [
+            'machine' => MachineFactory::random(),
+            'product' => ProductFactory::random(),
+            'quantity' => $faker->randomNumber(2),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Stock $stock): void {})
+        ;
+    }
+}
